10ca9551cbc0d9d0743003c7dff87d2f3196b9a8
mynameisVictoria <bemil1226@gmail.com>
2025-12-23 14:19:03 +0100

    Update main.py

--- src/server/main.py
+++ src/server/main.py
@@ -2,21 +2,34 @@ import socket
 
 #-------------SERVER----------#
 
+port = 1111
+
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-port = 1111
-
 my_socket.bind(("localhost", port))
 
+accept_state = False
 
+client = None
+address = None
 
 while True:
+
+
     my_socket.listen(5)
     try:
         client, address = my_socket.accept()
-        print("socket accepted")
-        #client.sendall(b"test sending from server \n")
-        print(client.recv(1024))
-    except socket.error:
-        continue
+        print(f"socked accepted, {client}, {address}")
+        accept_state = True
+    except Exception as err:
+        print(f"Error: {err}")
+
+    if accept_state:
+        client.sendall(b"test sending from server \n")
+        data = client.recv(1024)
+        if not data:
+            client.close()
+    elif not accept_state:
+        accept_state = False
+
