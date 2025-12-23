f051986a8038ca05507c62879bd9ecd5157a8b8a
mynameisVictoria <bemil1226@gmail.com>
2025-12-23 08:52:46 +0100

    Implement basic TCP server in main.py

--- src/server/main.py
+++ src/server/main.py
@@ -1,1 +1,22 @@
+import socket
 
+#-------------SERVER----------#
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+port = 1111
+
+my_socket.bind(("localhost", port))
+
+
+
+while True:
+    my_socket.listen(5)
+    try:
+        client, address = my_socket.accept()
+        print("socket accepted")
+        #client.sendall(b"test sending from server \n")
+        print(client.recv(1024))
+    except socket.error:
+        continue
