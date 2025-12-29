9845a0ca1c45c0da21e8ba7c25980a8e5ef49334
mynameisVictoria <bemil1226@gmail.com>
2025-12-29 18:07:20 +0100

    Implement TLS for socket connection and refactor code
    Refactor socket connection to use TLS and improve error handling.

--- src/client/main.py
+++ src/client/main.py
@@ -1,16 +1,18 @@
 import socket
-import errno
 import time
 import threading
 from queue import Queue
 from client_funcs import *
+import ssl
+import errno
 
 #-----------------CLIENT-------------------------#
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.settimeout(0.5)
+
+hostname = "p9cx.org"
+
+context = ssl.create_default_context()
 
 port = 1111
-
 send_info_queue = Queue(maxsize=10)   # thread safe data exchange
 
 storing = JsonStoring("user_data.json")
@@ -35,11 +37,11 @@ def handle_input():
         if send_info_input == "exit":
             break
 
-def constant_recv():
+def constant_recv(recv_socket):
     while True:
         time.sleep(0.1)
         try:
-            print(my_socket.recv(1024).decode())
+            print(recv_socket.recv(1024).decode())
         except socket.timeout:
             continue
         except Exception:
@@ -50,38 +52,43 @@ def main():
 
     while True:
         time.sleep(0.5)
-        if not connected:
-            try:
-                my_socket.connect(("127.0.0.1", port))
-                print("socket connected")
-                connected = True
-            except OSError as e:
-                if e.errno in (errno.EISCONN, 56):  #checks if its already connected
-                    print("ERROR")
-                    connected = True
-                else:   #idfk man
-                    connected = False
 
-        elif connected:
-            if send_info_queue.empty():  #if its empty, reset the loop
-                continue
-            elif not send_info_queue.empty():  #if it's not empty, try to send the data
-                try:
-                    send_data = storing.read_name() + ": " + send_info_queue.get()
-                    if send_data == "exit":
+        try:
+
+            my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            my_socket.settimeout(0.5)
+
+            tls_socket = context.wrap_socket(
+                my_socket,
+                server_hostname=hostname
+            )
+
+            tls_socket.connect((hostname, port))
+            print("socket connected")
+
+            recv_thread = threading.Thread(target=constant_recv, daemon=True, args=(tls_socket,))
+            recv_thread.start()
+
+            while True:
+                time.sleep(0.1)
+                if send_info_queue.empty():  #if its empty, reset the loop
+                    continue
+                elif not send_info_queue.empty():  #if it's not empty, try to send the data
+                    try:
+                        send_data = storing.read_name() + ": " + send_info_queue.get()
+                        if send_data == "exit":
+                            break
+                        else:
+                            tls_socket.sendall(send_data.encode("utf-8"))
+                    except (socket.error, OSError) as err:   #don't really know what can go wrong
                         break
-                    else:
-                        my_socket.sendall(send_data.encode("utf-8"))
-                        #print("sent")
-                except (socket.error, OSError) as err:   #don't really know what can go wrong
-                    print(err)
+
+        except OSError as err:
+            if err.errno in (errno.EISCONN, 9): #bad file descripter, closed/already connected
+                continue
 
 input_thread = threading.Thread(target=handle_input)
-recv_thread = threading.Thread(target=constant_recv, daemon=True)
-
 input_thread.start()   #starts the user input thread
-recv_thread.start()
 
 main()
 input_thread.join()
-recv_thread.join()
