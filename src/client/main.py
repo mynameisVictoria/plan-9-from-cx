1d4996f02c284733a6497b4eea9d697c2ece6fcc
mynameisVictoria <bemil1226@gmail.com>
2025-12-30 18:43:15 +0100

    Update main.py

--- src/client/main.py
+++ src/client/main.py
@@ -1,12 +1,11 @@
+import _thread
 import socket
-import time
+from time import sleep
 import threading
 from queue import Queue
 from client_funcs import *
 import ssl
-import errno
 from datetime import datetime, timezone
-
 #-----------------CLIENT-------------------------#
 
 hostname = "p9cx.org"
@@ -21,8 +20,9 @@ print("For more information check out the GitHub \n https://github.com/mynameisV
 
 storing = JsonStoring("user_data.json")
 if not storing.check_name():
-    name = input("Whats your name?")
+    name = input("Whats your name? \n")
     storing.write_name(name)
+    print("simply type, and press enter to transmit: \n")
 elif storing.check_name():
     name_decision = input(f"do you wish to change your name, current name: {storing.get_name()} \n y or n \n")
     if name_decision.lower() == "y":
@@ -36,16 +36,16 @@ elif storing.check_name():
 
 def handle_input():
     while True:
-        time.sleep(0.1)
-        with message_lock:
-            send_info_input = input("")
-            send_info_queue.put(send_info_input)
+        sleep(0.1)
+        send_info_input = input("")
+        if not send_info_input.strip() == "":
+            with message_lock:
+                send_info_queue.put(send_info_input)
         if send_info_input == "exit":
-            break
-
+            _thread.interrupt_main()
 def constant_recv(recv_socket):
     while True:
-        time.sleep(0.1)
+        sleep(0.1)
         try:
             print(recv_socket.recv(1024).decode())
         except socket.timeout:
@@ -60,11 +60,15 @@ def format_message(username, message):
 def main():
 
     while True:
-        time.sleep(0.5)
+        sleep(0.5)
 
         try:
-
             my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            # Source - https://stackoverflow.com/a
+            # Posted by Joshua Wolff
+            # Retrieved 2025-12-30, License - CC BY-SA 4.0
+            # noinspection PyUnresolvedReferences
             my_socket.settimeout(0.5)
 
             tls_socket = context.wrap_socket(
@@ -77,24 +81,19 @@ def main():
             recv_thread.start()
 
             while True:
-                time.sleep(0.1)
+                sleep(0.1)
                 if send_info_queue.empty():  #if its empty, reset the loop
                     continue
                 elif not send_info_queue.empty():  #if it's not empty, try to send the data
                     try:
                         with message_lock:
                             send_data = format_message(storing.get_name(), send_info_queue.get())
-                        if send_data == "exit":
-                            break
-                        else:
                             tls_socket.sendall(send_data.encode("utf-8"))
                     except (socket.error, OSError):   #don't really know what can go wrong
                         break
 
-        except OSError as err:
-            if err.errno in (errno.EISCONN, 9): #bad file descriptor
-                continue
-
+        except socket.error:
+            print("test")
 
 input_thread = threading.Thread(target=handle_input)
 input_thread.start()   #starts the user input thread
