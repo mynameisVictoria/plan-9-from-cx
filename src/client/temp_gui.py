4e045e920eb656814dfdcf9233ad00bf95b0568b
Elton2048 <eltontu2048@gmail.com>
2026-01-01 10:09:04 -0500

    Added constraints on message height and automatic scroll function to the bottom of the feed when new messages enter

--- src/client/temp_gui.py
+++ src/client/temp_gui.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QScrollArea, QWidget, QVBoxLayout, QLabel, QHBoxLayout
 from PyQt6.QtGui import QIcon
 from pathlib import Path
+from time import sleep
 
 
 class MainWindow(QMainWindow):
@@ -21,8 +22,8 @@ class MainWindow(QMainWindow):
         central = QWidget()
         main_layout = QVBoxLayout(central)
 
-        message_display_area = QScrollArea() # Initializes the scrolling area
-        message_display_area.setWidgetResizable(True)
+        self.message_display_area = QScrollArea() # Initializes the scrolling area
+        self.message_display_area.setWidgetResizable(True)
 
         container_for_messages = QWidget()
         self.layout_for_messages = QVBoxLayout(container_for_messages) # Creates the layout for the widget in the scrolling area
@@ -30,8 +31,8 @@ class MainWindow(QMainWindow):
         self.labels = []
         self.label_count = 0
 
-        message_display_area.setWidget(container_for_messages) # Throws the actual message widgets into the scrollinator area
-        main_layout.addWidget(message_display_area)
+        self.message_display_area.setWidget(container_for_messages) # Throws the actual message widgets into the scrollinator area
+        main_layout.addWidget(self.message_display_area)
 
         input_section = QHBoxLayout() # Creates and adds the input section to the main layout
         input_section.addWidget(self.input_field)
@@ -40,6 +41,7 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(central) # Throws the main_layout onto the entire window
 
+        self.message_display_area.verticalScrollBar().rangeChanged.connect(self.scroll_to_bottom) # Automatically scrolls to the bottom upon adding a message that changes FOV
         self.send_button.clicked.connect(self.send_message)
 
     def send_message(self):
@@ -48,10 +50,17 @@ class MainWindow(QMainWindow):
         self.input_field.clear()
         
     def receive_message(self, received_message:str):
-        label = QLabel(received_message)
+        label = QLabel(received_message) # Sets up each messages formatting!
+        label.setWordWrap(True)
+        label.setMinimumHeight(50)
+        label.setMaximumHeight(100)
+        # Adds the label to the message area
         self.layout_for_messages.addWidget(label)
         self.labels.append(label)
         self.label_count += 1
+
+    def scroll_to_bottom(self, _min=None, _max=None):
+        self.message_display_area.verticalScrollBar().setValue(self.message_display_area.verticalScrollBar().maximum())
 
 
 def main():
