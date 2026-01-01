36d6631a9cfd7509e2fdff26b7dcdec78a3e0110
mynameisVictoria <bemil1226@gmail.com>
2026-01-01 19:37:24 +0100

    Update temp_gui.py

--- src/client/temp_gui.py
+++ src/client/temp_gui.py
@@ -14,14 +14,23 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QScrollArea, QWidget, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QScrollArea, QWidget, QVBoxLayout, \
+    QLabel, QHBoxLayout
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import pyqtSignal
 from pathlib import Path
-from time import sleep
+import threading
+from queue import Queue
+from client_funcs import *
+import ssl
 
+context = ssl.create_default_context()
+HOST = "p9cx.org"
+
+message_queue = Queue(maxsize=10)   # thread safe data exchange
 
 class MainWindow(QMainWindow):
+    new_message_signal = pyqtSignal(str)
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Comms-Platform Client")
@@ -29,63 +38,99 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon(str(BASE_DIR / "icon.png")))
         self.send_button = QPushButton("Send")
         self.input_field = QLineEdit()
-
-
+        self.new_message_signal.connect(self.receive_message)
         self.initUI()
-    
 
-    def initUI(self): # Makes the UI and stuffs
+    def initUI(self):  # Makes the UI and stuffs
         central = QWidget()
         main_layout = QVBoxLayout(central)
 
-        self.message_display_area = QScrollArea() # Initializes the scrolling area
+        self.message_display_area = QScrollArea()  # Initializes the scrolling area
         self.message_display_area.setWidgetResizable(True)
 
         container_for_messages = QWidget()
-        self.layout_for_messages = QVBoxLayout(container_for_messages) # Creates the layout for the widget in the scrolling area
-        self.layout_for_messages.addStretch() # So the messages start at the bottom
+        self.layout_for_messages = QVBoxLayout(
+            container_for_messages)  # Creates the layout for the widget in the scrolling area
 
         self.labels = []
         self.label_count = 0
 
-        self.message_display_area.setWidget(container_for_messages) # Throws the actual message widgets into the scrollinator area
+        self.message_display_area.setWidget(
+            container_for_messages)  # Throws the actual message widgets into the scrollinator area
         main_layout.addWidget(self.message_display_area)
 
-        input_section = QHBoxLayout() # Creates and adds the input section to the main layout
+        input_section = QHBoxLayout()  # Creates and adds the input section to the main layout
         input_section.addWidget(self.input_field)
-        input_section.addWidget(self.send_button)   
+        input_section.addWidget(self.send_button)
         main_layout.addLayout(input_section)
 
-        self.setCentralWidget(central) # Throws the main_layout onto the entire window
+        self.setCentralWidget(central)  # Throws the main_layout onto the entire window
 
-        self.message_display_area.verticalScrollBar().rangeChanged.connect(self.scroll_to_bottom) # Automatically scrolls to the bottom upon adding a message that changes FOV
+        self.message_display_area.verticalScrollBar().rangeChanged.connect(
+            self.scroll_to_bottom)  # Automatically scrolls to the bottom upon adding a message that changes FOV
         self.send_button.clicked.connect(self.send_message)
 
     def send_message(self):
-        message = self.input_field.text() # THIS IS WHERE THE MESSAGE COMES OUT AS A STRING
-        self.receive_message(message)
-        self.input_field.clear()
-        
-    def receive_message(self, received_message:str):
-        label = QLabel(received_message) # Sets up each messages formatting!
+        message = self.input_field.text()  # THIS IS WHERE THE MESSAGE COMES OUT AS A STRING
+        message_queue.put(message)
+
+    def receive_message(self, message):
+        label = QLabel(message)  # Sets up each messages formatting!
         label.setWordWrap(True)
-        label.setMinimumHeight(25)
+        label.setMinimumHeight(50)
         label.setMaximumHeight(100)
         # Adds the label to the message area
-        self.layout_for_messages.addWidget(label, alignment=Qt.AlignmentFlag.AlignBottom)
+        self.layout_for_messages.addWidget(label)
         self.labels.append(label)
         self.label_count += 1
 
     def scroll_to_bottom(self, _min=None, _max=None):
         self.message_display_area.verticalScrollBar().setValue(self.message_display_area.verticalScrollBar().maximum())
 
-
 def main():
     app = QApplication(sys.argv)
+    global window
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
+def network_main():
+    while True:
+        sleep(0.1)
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.settimeout(0.5)
+
+            tls_socket = context.wrap_socket(
+                client_socket,
+                server_hostname=HOST
+            )
+            network = NetworkIO(tls_socket)
+            network.try_connect()
+
+            while True:
+                sleep(0.1)
+
+                server_sent_data = network.socket_receive()
+                if not server_sent_data:
+                    print("server not received data")
+                elif type(server_sent_data) == str:
+                    window.new_message_signal.emit(server_sent_data)
+                    print("new message sent")
+
+                if not message_queue.empty():  # if it's not empty, try to send the data
+                    try:
+                        network.socket_send(message_queue.get())
+                        print("nessage queuue thingy")
+                    except (socket.timeout, OSError):
+                        print("it BREAKS")
+                        break
+
+        except socket.error:
+            continue
+
+network_thread = threading.Thread(target=network_main)
+network_thread.start()
 
 if __name__ == "__main__":
     BASE_DIR = Path(__file__).resolve().parent
