9ab535e6906719e50c8ccbd44f98ab071bde9bbe
mynameisVictoria <bemil1226@gmail.com>
2026-01-10 12:07:12 +0100

    Update tui.py

--- src/client/tui.py
+++ src/client/tui.py
@@ -1,23 +1,25 @@
 import time
-
 from prompt_toolkit.application import Application
 from prompt_toolkit.document import Document
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.widgets import TextArea
-import queue
 from client_funcs import *
+from dataclasses import dataclass
 import threading
 import os
 
+@dataclass
+class NVals:
+    HOSTNAME: str
+    PORT: int
+
+DFLNVals = NVals("p9cx.org", 1111)
+
 kb = KeyBindings()
-
-send_message = queue.Queue()
+send_message = []
 send_lock = threading.Lock()
-
-HOSTNAME = "p9cx.org"
-PORT = 1111
 
 def const_receive(recv_socket):
     pass
@@ -26,16 +28,16 @@ def network_main():
     while True:
         sleep(0.5)
         try:
-            network = Network(HOSTNAME, PORT)
+            network = Network(DFLNVals.HOSTNAME, DFLNVals.PORT)
             network.tls_socket_creation()
             network.connect()
 
             while True:
                 sleep(0.1)
-                if not send_message.empty():  # if it's not empty, try to send the data
+                if len(send_message) != 0:  # if it's not empty, try to send the data
                     try:
                         with send_lock:
-                            message = send_message.get()
+                            message = send_message.pop(0)
                             network.socket_sendall(str(message))
                     except (socket.error, OSError):
                         break
@@ -68,14 +70,10 @@ class Gui:
         except BaseException as e:
             output = f"\n\n{e}"
 
-        new_text = self.output_field.text + output + "\n"
+        new_text = self.output_field.text + output
 
         with send_lock:
-            send_message.put(new_text)
-
-        self.output_field.buffer.document = Document(
-            text=new_text
-        )
+            send_message.append(new_text)
 
     def main(self):
         self.input_field.accept_handler = self.accept
